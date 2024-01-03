ad7c66e7c11cedf27d67481e2bcbe032a33b4b5b
ctx-devansh <devansh.the.photofreak@gmail.com>
2024-01-03 12:57:28 -0800

    Trying Theme functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
 import './App.css';
-import {TextField} from '@mui/material';
+import {Button, TextField} from '@mui/material';
 import NumberButton from './widgets/NumberButton';
 import OperationButton from './widgets/OperationButton';
 function App() {
+
+  var currentTheme = "type1";
+
+  function handleClick(){
+    if(currentTheme === "type1"){
+      currentTheme = "type2";
+    }else{
+      currentTheme = "type1";
+    }
+    console.log(currentTheme);
+    const table = document.querySelectorAll("#buttonsTable");
+    console.log(table.childNodes);
+    for(const row in table.childNodes){
+      console.log(row);
+      for(const button in row.childNodes){
+        console.log(button);
+        button.setAttribute("type",currentTheme);
+      }
+    }
+  }
+
   return (
       <div id="main">
+        <Button onClick={handleClick}>Light/Dark</Button>
         <TextField id='calcTextField' className='calc' label="" variant="outlined" ></TextField>
         <table id="buttonsTable">
           <tr>
-            <OperationButton value="AC"></OperationButton>
-            <NumberButton value="(" />
-            <NumberButton value=")"></NumberButton>
-            <OperationButton value="/"></OperationButton>
+            <OperationButton id="button" value="AC" type={currentTheme}></OperationButton>
+            <NumberButton id="button" value="(" type={currentTheme}/>
+            <NumberButton id="button" value=")" type={currentTheme}></NumberButton>
+            <OperationButton id="button" value="/" type={currentTheme}></OperationButton>
           </tr>
           <tr>
-            <NumberButton value="7"></NumberButton>
-            <NumberButton value="8"></NumberButton>
-            <NumberButton value="9"></NumberButton>
-            <OperationButton value="*"></OperationButton>
+            <NumberButton id="button" value="7" type={currentTheme}></NumberButton>
+            <NumberButton id="button" value="8" type={currentTheme}></NumberButton>
+            <NumberButton id="button" value="9" type={currentTheme}></NumberButton>
+            <OperationButton id="button" value="*" type={currentTheme}></OperationButton>
           </tr>
           <tr>
-            <NumberButton value="4"></NumberButton>
-            <NumberButton value="5"></NumberButton>
-            <NumberButton value="6"></NumberButton>
-            <OperationButton value="-"></OperationButton>
+            <NumberButton id="button" value="4" type={currentTheme}></NumberButton>
+            <NumberButton id="button" value="5" type={currentTheme}></NumberButton>
+            <NumberButton id="button" value="6" type={currentTheme}></NumberButton>
+            <OperationButton id="button" value="-" type={currentTheme}></OperationButton>
           </tr>
           <tr>
-            <NumberButton value="1"></NumberButton>
-            <NumberButton value="2"></NumberButton>
-            <NumberButton value="3"></NumberButton>
-            <OperationButton value="+"></OperationButton>
+            <NumberButton id="button" value="1" type={currentTheme}></NumberButton>
+            <NumberButton id="button" value="2" type={currentTheme}></NumberButton>
+            <NumberButton id="button" value="3" type={currentTheme}></NumberButton>
+            <OperationButton id="button" value="+" type={currentTheme}></OperationButton>
           </tr>
 
           <tr>
-            <NumberButton value="0"></NumberButton>
-            <NumberButton value="."></NumberButton>
-            <OperationButton value="="></OperationButton>
+            <NumberButton id="button" value="0" type={currentTheme}></NumberButton>
+            <NumberButton id="button" value="." type={currentTheme}></NumberButton>
+            <OperationButton id="button" value="=" type={currentTheme}></OperationButton>
           </tr>
         </table>
       </div>
--- src/widgets/NumberButton.js
+++ src/widgets/NumberButton.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import {Button} from '@mui/material';
+import { ThemeProvider,createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const type1 = createTheme({
+  palette: {
+    color1: {
+      main: 'red',
+      light: 'green',
+      dark: 'blue',
+      contrastText: '#FFFFFF',
+    },
+  },
+});
+
+const type2 = createTheme({
+    palette: {
+      color1: {
+        main: '#4b4b4b',
+        light: '#4b4b4b',
+        dark: '#717171',
+        contrastText: '#FFFFFF',
+      },
+    },
+  });
 
 function NumberButton(props){
     function handleClick(){
         console.log("hello");
         document.getElementById('calcTextField').value += props.value;
     }
-    return(
-        <Button onClick={handleClick} sx={[{height: '60px', width: '60px', bgcolor: 'black', borderRadius: '50%', color: 'white', margin: '5px'},{'&:hover': {backgroundColor: 'grey',}}]}>
-            {props.value}
-        </Button>
-    );
+
+    if(props.type === "type1"){
+        return(
+            <ThemeProvider theme={type1}>
+                <CssBaseline />
+                <Button variant="contained" onClick={handleClick} color="color1">
+                    {props.value}
+                </Button>
+            </ThemeProvider>
+        );
+    }else{
+        return(
+            <ThemeProvider theme={type2}>
+                <CssBaseline />
+                <Button variant="contained" onClick={handleClick} color="color1">
+                    {props.value}
+                </Button>
+            </ThemeProvider>
+        );
+    }
+
+
 }
 export default NumberButton;
